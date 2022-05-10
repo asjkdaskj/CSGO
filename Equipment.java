bc6cc8f966cc07bfaab3b551dc513d4c00b04c78
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-05-10 10:09:13 -0400

    Update and rename Equipment to Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -5,11 +5,13 @@
 */
 public class Equipment extends Item {
 
-  public Equipment (double cost, String name) {
-    super(cost, name);
+  public Equipment (double cost, String name, boolean isBought) {
+    super(cost, name,isBought);
   }
 
   public void useKit() {
     //use kit idk defusedbomb = true
+
+    // if no kit defuse = 10 seconds , if kit defuse = 5 seconds
   }
 }
