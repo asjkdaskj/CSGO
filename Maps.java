5dd2c72720f78fcc0e52cf209de909244c2b1af6
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-06-14 13:04:27 -0400

    Update and rename Maps to Maps.java

--- Maps.java
+++ Maps.java
@@ -4,7 +4,13 @@
 * description: Maps superclass for adding maps in the game
 */
 public class Maps {
+  
+  //attributes
+  
+  /** map name */
   private String name;
+  
+  /** callouts */
   private String callout1;
   private String callout2;
   private String callout3;
@@ -12,6 +18,17 @@ public class Maps {
   private String callout5;
   private String callout6;
 
+  /** 
+  * creating Map
+  *
+  * @param name, map name
+  * @param callout 1, first callout
+  * @param callout 2, second callout
+  * @param callout 3, third callout
+  * @param callout 4, forth callout
+  * @param callout 5, fifth callout
+  * @param callout 6, sixth callout
+  */
   public Map (String name, String callout1, String callout2, String callout3, String callout4, String callout6) {
     this.name = name;
     this.callout1 = callout1; 
